dec6eb376e86277369379d8b0bdbab8ed18f3b7c
RMS-Ronaldo <ronaldo7472_markes@hotmail.com>
2025-09-08 20:10:42 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,13 @@
 Console.WriteLine("Hello, World!");
 
 Televisao tv = new Televisao(55f);
-Console.WriteLine ($"A tv tem o tamanho {tv.Tamanho}");
+Console.WriteLine($"A tv tem o tamanho {tv.Tamanho}");
+
+tv.Volume = -35;
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.Volume = 120;
+Console.WriteLine($"Volume {tv.Volume}");
 
 tv.AumentarVolume();
-Console.WriteLine ($"Volume {tv.Volume}");
+Console.WriteLine($"Volume {tv.Volume}");
